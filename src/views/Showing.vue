12cb01acbb565185db9c58a1475a76ecdf530607
dustinleong-ipm <dustinleong@ipm.edu.mo>
2021-06-01 14:41:13 +0800

    test05

--- src/views/Showing.vue
+++ src/views/Showing.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an showing page</h1>
+    <form class="pure-form">
+      <input type="text" v-model="firstName"/>
+      <input type="text" v-model="lastName"/>
+    </form>
+    <p>{{fullName}}</p>
   </div>
 </template>
+<script>
+export default {  
+  data() {
+      const firstName= 'PJ'
+      const lastName= 'Chen'
+    return{
+      firstName,lastName
+    }
+  },
+  computed:{
+    fullName(){
+      console.log('computed getter')
+      return this.firstName+this.lastName
+    }
+  }
+}
+</script>
